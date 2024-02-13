8bcf13a89a4969429598dd27467001562bfa6f3b
manuel_859 <manuel_859@outlook.es>
2024-02-13 14:31:55 -0600

    inicialización del servidor en el puerto 3000, creación de verbos

--- e-commerce/server.js
+++ e-commerce/server.js
@@ -1,1 +1,16 @@
-console.log("Starting")
+const express = require("express");
+const mongoose = require('mongoose');
+const dotenv = require("dotenv").config();
+const authRoutes = require('./routes/authRouttes');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//verbos
+app.get('/api/cliente', (req, res) => {
+    res.status(500).json({mensaje: 'Get cliente'});
+});
+
+app.listen(port, () => {
+    console.log(`Servidor iniciado en el puerto ${port}`);
+});
