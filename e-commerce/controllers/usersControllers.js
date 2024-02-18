57a4477f38cebe9f656199349c68446594f49bc0
manuel_859 <manuel_859@outlook.es>
2024-02-18 16:43:54 -0600

    creación de usuarios todo de forma correcta

--- e-commerce/controllers/usersControllers.js
+++ e-commerce/controllers/usersControllers.js
@@ -3,31 +3,54 @@ const bcrypt = require('bcrypt');
 const asyncHandler = require('express-async-handler')
 const User = require('../models/usersModel')
 
-const crearUser = asyncHandler( async (req, res) => {
+const crearUser = asyncHandler(async (req, res) => {
+    try {
+        // Crear un nuevo usuario en la base de datos con el body que le pasamos
+        const { username, email, password } = req.query;
 
-    //Crear un nuevo usuario en la base de datos con el body que le pasamos
-   const { name, email, password} = req.query
-    
-    if(!name || !email || !password){
-        res.status(400)
-        throw new Error('Faltan datos')
+        if (!username || !email || !password) {
+            res.status(400);
+            throw new Error('Faltan datos');
+        }
+
+        // Verificar que ese usuario no exista a través de su email
+        const userExiste = await User.findOne({ email });
+        if (userExiste) {
+            res.status(400);
+            throw new Error('El usuario ya está registrado con ese correo');
+        }
+
+        // Hacer el Hash al password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        console.log(hashedPassword);
+
+        // Crear el usuario
+        const user = await User.create({
+            username,
+            email,
+            password: hashedPassword
+        });
+
+        if (user) {
+            res.status(201).json({
+                _id: user.id,
+                username: user.username,
+                email: user.email,
+                password: hashedPassword
+            });
+        } else {
+            res.status(400);
+            throw new Error('Error interno del servidor, no se puede registrar el usuario');
+        }
+    } catch (error) {
+        // Manejar errores
+        res.status(500).json({ error: error.message });
     }
+});
 
-    
 
-    //verificar que ese usuario no exista a traves de su email
-    const userExiste = await User.findOne({email})
-    if(userExiste){
-        res.status(400)
-        throw new Error ('El usuario ya esta registrado con ese correo')
-    }
 
-    // Hacemos el Hash al password
-    //const salt = await 
-
-    res.status(201).json({message: 'Crear Usuario' })
-
-})
 
 const loginUser =asyncHandler(  async(req, res) => {
     res.status(201).json({message: 'Login del usuario' })
